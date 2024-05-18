8c197b705cce8036afd9435bf600c984bfe9c69b
Pop Alexandru Bogdan <alexpop121314@gmail.com>
2024-05-18 10:17:52 +0300

    corrected oop exam2

--- oop/test2/main.cpp
+++ oop/test2/main.cpp
@@ -8,6 +8,7 @@ int main() {
     Volume volume1("Volume 1", 10.99, 4.5, 2022);
     Volume volume2("Volume 2", 12.99, 4.2, 2023);
     Volume volume3("Volume 3", 14.99, 4.7, 2024);
+    Volume volume4("volume 4", 11.99, 3.5, 1999);
 
     Series series("Adventure Series");
     series.addVolume(&volume1);
@@ -17,10 +18,11 @@ int main() {
     bookstore.addItem(&volume1);
     bookstore.addItem(&volume2);
     bookstore.addItem(&volume3);
+    bookstore.addItem(&volume4);
     bookstore.addItem(&series);
 
     std::cout << "Items with worldbuilding quality greater than 4.0:" << std::endl;
-    bookstore.displayItemsWithWorldBuildingQuality(4.0);
+    bookstore.displayItemsWithWorldBuildingQuality(4.5);
 
     std::cout << "\nAll items in the bookstore:" << std::endl;
     std::cout << bookstore << std::endl;
